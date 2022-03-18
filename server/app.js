3e38c70a48776ef56270bdd156f4d17733158de8
yuhcee <egbouchenna001@gmail.com>
2022-03-18 23:36:00 +0100

    add requestListener for guitars data.

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 const http = require('http');
+const guitars = require('./guitars.json');
 
-const host = 'localhost',
-    port = 8080;
+const host = 'localhost';
+const port = 8080;
 
-const listProducts = (req, res) => {};
+const listProducts = function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.writeHead(200, 'OK');
+    res.end(JSON.stringify(guitars, null, 2));
+};
 
 const server = http.createServer(listProducts);
 server.listen(port, host, () => {
-    `Server is running on : http://${localhost}:${port}`;
+    console.log(`Server is running on : http://${host}:${port}`);
 });
