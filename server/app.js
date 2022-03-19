bce2234b12d785ed1fef1f761638faae4cfd58da
yuhcee <egbouchenna001@gmail.com>
2022-03-19 03:07:28 +0100

    allow control access for client

--- server/app.js
+++ server/app.js
@@ -6,11 +6,13 @@ const port = 8080;
 
 const listProducts = function (req, res) {
     res.setHeader('Content-Type', 'application/json');
+    res.setHeader(`Access-Control-Allow-Origin`, `http://${host}:3000`);
     res.writeHead(200, 'OK');
     res.end(JSON.stringify(guitars, null, 2));
 };
 
 const server = http.createServer(listProducts);
+
 server.listen(port, host, () => {
     console.log(`Server is running on : http://${host}:${port}`);
 });
